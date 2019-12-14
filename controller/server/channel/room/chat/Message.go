e8cfdc8bc8933127bdd8c1e96f9b62688be1ffcc
Yanlong-li <ahlyl94@gmail.com>
2019-12-14 13:12:23 +0800

    暂时放弃去中心化架构 增加消息群发

--- controller/server/channel/room/chat/Message.go
+++ controller/server/channel/room/chat/Message.go
@@ -14,7 +14,7 @@ func init() {
 func TextMessage(msg message.SendTextMessage, conn connect.Connector) {
 	fmt.Println("收到消息")
 	fmt.Println(msg)
-	conn.Send(message.TextMessage{
+	conn.Broadcast(message.TextMessage{
 		SendTextMessage: msg,
 		Time:            201900000000,
 		Author: struct {
@@ -24,5 +24,5 @@ func TextMessage(msg message.SendTextMessage, conn connect.Connector) {
 			Id:       "user-2",
 			Username: "测试1好",
 		},
-	})
+	}, true)
 }
---
+++ packet/server/List.go
@@ -0,0 +1,6 @@
+package server
+
+// 服务器列表
+type List struct {
+	List []Info
+}
