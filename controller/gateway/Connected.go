60e7f24c6a07f4f34a0a98d08d68318b01029225
Yanlongli <ahlyl94@gmail.com>
2020-11-20 16:59:03 +0800

    负载上报，ip获取

--- controller/gateway/Connected.go
+++ controller/gateway/Connected.go
@@ -6,21 +6,40 @@ import (
 	"github.com/yanlong-li/hi-go-gateway/packet_model"
 	"github.com/yanlong-li/hi-go-gateway/packet_model/server"
 	logger "github.com/yanlong-li/hi-go-logger"
+	common2 "github.com/yanlong-li/hi-go-server/common"
 	"github.com/yanlong-li/hi-go-socket/connect"
 	"github.com/yanlong-li/hi-go-socket/route"
-	"net"
+	"time"
 )
 
 func init() {
 	route.Register(common.GatewayAndServerGroup, packet_model.Connected{}, Connected)
+	route.Register(common.GatewayAndServerGroup, server.RegisterSuccess{}, func(success server.RegisterSuccess, conn connect.Connector) {
+		fmt.Println("网关注册成功！")
+
+		for {
+			time.Sleep(time.Second * 5)
+			err := conn.Send(server.LoadReport{CurrentLoad: connect.Count()})
+			if err != nil {
+				break
+			}
+		}
+	})
 }
 
 func Connected(conn connect.Connector) {
 	logger.Debug("连接到网关", 0, conn.GetId())
 
-	fmt.Println(net.InterfaceAddrs())
+	common2.Client = conn
 
-	a := net.ParseIP("127.0.0.1")
-
-	_ = conn.Send(server.Register{Name: "s1", Version: "v1", IP: a, Port: 3002, PeakLoad: 50000, OptimumLoad: 10000, Weight: 1})
+	_ = conn.Send(
+		server.Register{
+			Name:        "s1",
+			Version:     "1.0.0",
+			IP:          common2.ServerListenServer.IP,
+			Port:        common2.ServerListenServer.Port,
+			PeakLoad:    50000,
+			OptimumLoad: 10000,
+			Weight:      100,
+		})
 }
--- main.go
+++ main.go
@@ -4,21 +4,46 @@ import (
 	"fmt"
 	"github.com/yanlong-li/hi-go-gateway/common"
 	logger "github.com/yanlong-li/hi-go-logger"
+	common2 "github.com/yanlong-li/hi-go-server/common"
 	_ "github.com/yanlong-li/hi-go-server/controller"
 	"github.com/yanlong-li/hi-go-socket/connect"
 	"github.com/yanlong-li/hi-go-socket/socket"
+	"net"
+	"strings"
 	"time"
 )
 
 func main() {
+	logger.SetLevel(logger.INFO)
 
-	logger.SetLevel(logger.ALL)
-	// 连接网关服务器
-	go socket.Client(common.GatewayAndServerGroup, "127.0.0.1:3000")
+	address, err := net.InterfaceAddrs()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	ip := address[1]
+	index := strings.Index(ip.String(), "/")
+
+	server := common2.Server{
+		IP:   net.ParseIP(ip.String()[:index]),
+		Port: 3002,
+	}
+	common2.ServerListenServer = server
+
+	go gateway()
 
 	// 接受客户端连接
 	go socket.Server(common.ServerAndClientGroup, ":3002")
 	printCount()
+}
+
+func gateway() {
+	defer func() {
+		gateway()
+	}()
+	// 连接网关服务器
+	socket.Client(common.GatewayAndServerGroup, "gateway:3000")
 }
 
 func printCount() {
